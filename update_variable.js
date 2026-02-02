e4e7a3ad28cfa088f478af038392c15761beb467
rohanchauhanignek-a11y <rohan.chauhan.ignek@gmail.com>
2026-02-02 16:08:12 +0530

    xyz

---
+++ update_variable.js
@@ -0,0 +1,10 @@
+//Use assignment operators to update a variable.
+function updata(a)
+    {
+        a+=10;
+        console.log(`After updateing value: ${a}`)
+    }
+let a=1;
+console.log(`Before updateing value: ${a}`)
+updata(a);
+
